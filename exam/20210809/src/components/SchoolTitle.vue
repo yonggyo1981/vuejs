c420499b01b6ce4be3ace10e6a8aa8c595a4e62c
YONGGYO LEE <yonggyo1981@gmail.com>
2021-08-10 08:48:54 +0900

    .

---
+++ exam/20210809/src/components/SchoolTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title : "학교 홈페이지 제목..."
+        };
+    }
+}
+</script>
---
+++ exam/20210809/src/views/School.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>School.vue</h1>
+    <h2 @click="changeTitle">{{ title }}</h2>
+    <SchoolTitle />
+    <school-title />
+</template>
+
+<script>
+import SchoolTitle from "../components/SchoolTitle.vue"
+export default {
+    components : {SchoolTitle},
+    data() {
+        return {
+            title : "학교 홈페이지"
+        }
+    },
+    created() {
+        console.log("Created");
+    },
+    mounted() {
+        console.log("Mounted");
+    },
+    unmounted() {
+        console.log("Unmounted");
+    },
+    methods : {
+        changeTitle() {
+            this.changeTitle2();
+        },
+        changeTitle2() {
+            this.title = "(변경)학교 홈페이지";
+        }
+    }
+}
+</script>
