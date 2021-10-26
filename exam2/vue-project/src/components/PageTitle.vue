be3dd0be9d80f5e72430be7d8f45c594f9502369
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-26 16:14:37 +0900

    .

---
+++ exam2/vue-project/src/components/PageTitle.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>제목....</h1>
+</template>
---
+++ exam2/vue-project/src/views/Test.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>Test 페이지....</h1>
+    <PageTitle />
+    <page-title />
+    <h1>{{ title }}</h1>
+</template>
+<script>
+import PageTitle from "../components/PageTitle.vue"
+export default {
+ name : "컴포넌트의 이름(생략가능)",
+ components : {PageTitle},
+ data() {
+     return {
+         title : "제목Title"
+     };
+ },
+ setup() {
+     // Composition API - vue3
+ },
+ created() {
+    console.log("Created!!");
+ },
+ mounted() { // HTML이 로딩이 완료 되었을때 
+    console.log("mounted!!");
+    this.msg();
+ },
+ unmounted() { // 페이지를 이동될때 호출
+    console.log("unmounted!!");
+ },
+ methods : {
+     msg() {
+         alert(this.title);
+     }
+ }
+}
+</script>
