fd4379493eea80ced68d7d13632825c310828156
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-29 15:59:56 +0900

    .

---
+++ exam2/vue-project/src/views/CompositionAPI2.vue
@@ -0,0 +1,28 @@
+<template>
+    <input type="text" v-model.number="state.num1"> + 
+    <input type="text" v-model.number="state.num2"> = 
+    {{ state.result }}
+</template>
+<script>
+import { reactive, computed, toRefs } from "vue";
+
+function calculator() {
+    let state = reactive({
+         num1 : 0,
+         num2 : 0,
+        result : computed(() => state.num1 + state.num2),
+    });
+
+    return toRefs(state);
+}
+
+export default {
+    setup() {
+        
+        let state = calculator();
+        return {
+            state,
+        };
+    }
+}
+</script>
