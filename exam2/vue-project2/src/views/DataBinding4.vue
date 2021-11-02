c8ae3429a3c11d1ea7511cb960cfbccf8f83da27
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-02 17:35:15 +0900

    .

--- exam2/vue-project2/src/views/DataBinding4.vue
+++ exam2/vue-project2/src/views/DataBinding4.vue
@@ -11,16 +11,33 @@
     <label>
         <input type="checkbox" v-model="checked" true-value="yes" false-value="no">{{ checked }}
     </label>
-    
+    <br><br>
+    <h1>좋아하는 과일은?</h1>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Apple">Apple
+    </label>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Orange">Orange
+    </label>
+    <label>
+        <input type="checkbox" v-model="fruits" value="Melon">Melon
+    </label>
+    <button type="button" @click="checkValue">확인</button>
 </template>
 <script> 
 export default {
     data() {
         return {
+            fruits : [],
             checked : "yes", // true, no -> false
             message : "textarea에 입력될 문장....",
             city : "032",
         };
+    },
+    methods : {
+        checkValue() {
+            console.log(this.fruits);
+        }
     }
 }
 </script>
