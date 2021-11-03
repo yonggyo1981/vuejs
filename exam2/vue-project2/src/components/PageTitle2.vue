65d009754d8b5d660858f3a305be35e898676cb0
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-03 16:38:47 +0900

    .

--- exam2/vue-project2/src/components/PageTitle2.vue
+++ exam2/vue-project2/src/components/PageTitle2.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>{{ title }}</h1>
-    
+    <h2>{{ posts.author }} {{ posts.title }}</h2>
 </template>
 <script>
 export default {
@@ -8,7 +8,42 @@ export default {
         title : {
             type : String,
             default : "기본 제목...."
+        },
+        num1 : {
+            type : Number,
+            default : 0,
+        },
+        num2 : {
+            type : Number,
+            default : 0,
+        },
+        checked : {
+            type : Boolean,
+            default : false,
+        },
+        fruits : {
+            type : Array,
+            default() {
+                return ["apple", "orange", "melon"];
+            }
+        },
+        posts : {
+            type : Object,
+            default() {
+                return { author : "작성자", title : "제목" };
+            }
         }
+    },
+    mounted() {
+        /*
+        const result = this.num1 + this.num2;
+        console.log(typeof this.num1);
+        console.log(typeof this.num2);
+        console.log(result);
+        */
+       //console.log(typeof this.checked);
+       console.log(this.fruits instanceof Array);
+       console.log(this.fruits);
     }
 }
 </script>
--- exam2/vue-project2/src/views/NestedComponent.vue
+++ exam2/vue-project2/src/views/NestedComponent.vue
@@ -1,10 +1,23 @@
 <template>
-    <PageTitle title="부모 컴포넌트 제목1" />
-    <page-title title="부모 컴포넌트 제목2" />
+    <PageTitle title="부모 컴포넌트 제목1" 
+        :num1="10" :num2="20"
+        :checked="true"
+        :fruits="['orange', 'banana', 'water melon']"
+        :posts="posts"
+    />
 </template>
 <script>
 import PageTitle from "../components/PageTitle2.vue";
 export default {
-    components : {PageTitle}
+    components : {PageTitle},
+    data() {
+        return {
+            posts : { 
+                author : "작성자3",
+                title : "제목3"
+            }
+        };
+    }
+
 }
 </script>
