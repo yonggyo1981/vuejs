2a82ae2ce7dcf8282657caad538361a4ec8e082e
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-04 18:09:17 +0900

    .

--- exam2/vue-kanban/src/components/member/Form.vue
+++ exam2/vue-kanban/src/components/member/Form.vue
@@ -1,3 +1,20 @@
 <template>
-가입/수정 양식
+<form method="post" autocomplete='off'>
+    <input type="text" name="memId" placeholder='아이디'><br>          
+    <input type="password" name="memPw" placeholder='비밀번호'><br>        
+    <input type="password" name="memPwRe" placeholder='비밀번호확인'><br>
+    <input type="text" name="memNm" placeholder='회원명'><br>
+    <input type="text" name="cellPhone" placeholder="휴대전화번호"><br>
+    <button type="submit">가입하기</button>
+</form>
 </template>
+<script>
+export default {
+    props : {
+        mode : {
+            type : String,
+            default : "join",
+        }
+    }
+}
+</script>
--- exam2/vue-kanban/src/views/member/Join.vue
+++ exam2/vue-kanban/src/views/member/Join.vue
@@ -1,11 +1,16 @@
 <template>
     <PageTitle>회원가입</PageTitle>
-    <Form />
+    <Form :mode="mode" />
 </template>
 <script>
 import PageTitle from "../../components/PageTitle.vue"
 import Form from "../../components/member/Form.vue"
 export default {
-    components: {PageTitle, Form} 
+    components: {PageTitle, Form},
+    data() {
+        return {
+            mode : "join",
+        };
+    }
 }
 </script>
