efda70f1654bed016488f5b147b7d11746f0f0b7
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-05 14:28:44 +0900

    .

--- exam2/vue-kanban/src/components/member/Form.vue
+++ exam2/vue-kanban/src/components/member/Form.vue
@@ -1,12 +1,12 @@
 <template>
 <form method="post" autocomplete='off'>
-    <input type="text" name="memId" placeholder='아이디'><br>          
+    <input type="text" name="memId" placeholder='아이디' :value="member.memId"><br>          
     <input type="password" name="memPw" placeholder='비밀번호'><br>        
     <input type="password" name="memPwRe" placeholder='비밀번호확인'><br>
     <input type="text" name="memNm" placeholder='회원명'><br>
     <input type="text" name="cellPhone" placeholder="휴대전화번호"><br>
-    <button type="submit" v-if="mode == 'join'">가입하기</button>
-    <button type="submit" v-else>수정하기</button>
+    <input type="submit" value="가입하기" v-if="mode == 'join'">
+    <input type="submit" value="수정하기" v-else>
 </form>
 </template>
 <script>
@@ -14,7 +14,21 @@ export default {
     props : {
         mode : {
             type : String,
-            default : "join2",
+            default : "join"
+        },
+        member : {
+            memId : {
+                type : String,
+                default : "아이디",
+            },
+            memNm : {
+                type : String,
+                default : "회원명",
+            },
+            cellPhone : {
+                type : String,
+                default:"0000",
+            }
         }
     }
 }
