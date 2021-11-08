19364ed22596fa0352526adbc43c74732e2ef1a4
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-08 15:56:55 +0900

    .

--- exam2/kanbanServer/models/member.js
+++ exam2/kanbanServer/models/member.js
@@ -1,5 +1,6 @@
 const { sequelize, Sequelize : { QueryTypes } } = require("./index");
- 
+const bcrypt = require('bcrypt');
+
 /**
 * 회원 models
 *
@@ -10,13 +11,32 @@ const member = {
 	*
 	*/
 	async join(data) {
-		const sql = `INSERT INTO member (memId, memPw, memNm, cellPhone) 
-								VALUES (:memId, :memPw, :memNm, :cellPhone)`;
-								
-		const result = await sequelize.query(sql, {
-				replacements : replacements,
-				type : QueryType.INSERT,
-		});
+		try {
+			const sql = `INSERT INTO member (memId, memPw, memNm, cellPhone) 
+									VALUES (:memId, :memPw, :memNm, :cellPhone)`;
+			const hash = data.memPw?await bcrypt.hash(data.memPw, 10):"";
+			let cellPhone = "";
+			if (data.cellPhone) {
+				cellPhone = data.cellPhone.replace(/[^0-9]/g, "");
+			}
+			const replacements = {
+				memId : data.memId,
+				memPw : hash,
+				memNm : data.memNm,
+				cellPhone,
+			};
+
+			const result = await sequelize.query(sql, {
+					replacements : replacements,
+					type : QueryTypes.INSERT,
+			});
+			
+			const memNo = result[0];
+			const memberInfo = await this.get(memNo);
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
 	},
 	/**
 	* 회원정보 수정
@@ -24,6 +44,34 @@ const member = {
 	*/
 	update(data) {
 		
+	},
+	/**
+	* 회원 정보 조회
+	*
+	* @param memNo - 정수 - 회원번호, 문자 -> 아이디
+	*/
+	async get(memNo) {
+		try {
+			let fields = "memNo";
+			if (typeof memNo == 'string') {
+				fields = "memId";
+			}
+			
+			const sql = `SELECT * FROM member WHERE ${fields} = ?`;
+			const rows = await sequelize.query(sql, {
+					replacements : [memNo],
+					type : QueryTypes.SELECT,
+			});
+			
+			if (rows.length == 0) { // 회원이 없는 경우 
+				return false;
+			}
+			
+			return rows[0];
+		} catch (err) {
+			console.error(err);
+			return false;
+		}
 	}
 };
 
--- exam2/kanbanServer/routes/member.js
+++ exam2/kanbanServer/routes/member.js
@@ -1,13 +1,13 @@
 const member = require("../models/member");
 const router = require('express').Router();
 
-router.use((req, res) => {
+router.use(async (req, res) => {
 	const mode = req.body.mode || req.query.mode;
 	const data = req.body.mode?req.body:req.query;
 	
 	switch(mode) {
 		case "join": // 회원가입 처리 
-			member.join(data);
+			await member.join(data);
 			break;
 		case "update": // 회원정보 수정
 			member.update(data);
