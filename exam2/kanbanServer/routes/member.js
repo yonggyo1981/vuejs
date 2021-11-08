85ca52ef126d823c3b1453f2bf4181e4b8dfe965
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-08 16:23:09 +0900

    .

--- exam2/kanbanServer/routes/member.js
+++ exam2/kanbanServer/routes/member.js
@@ -5,16 +5,33 @@ router.use(async (req, res) => {
 	const mode = req.body.mode || req.query.mode;
 	const data = req.body.mode?req.body:req.query;
 	
-	switch(mode) {
-		case "join": // 회원가입 처리 
-			await member.join(data);
-			break;
-		case "update": // 회원정보 수정
-			member.update(data);
-			break;
+	let success = false;
+	let returnData = {};
+	let message = "";
+	try {
+		switch(mode) {
+			case "join": // 회원가입 처리 
+				const memberInfo = await member.join(data);
+				if (memberInfo) {
+					success = true;
+					returnData = memberInfo;
+				} else { // 회원 가입 실패
+					throw new Error("회원가입 실패");
+				}
+				break;
+			case "update": // 회원정보 수정
+				member.update(data);
+				break;
+		}
+	} catch (err) {
+		message = err.message;
 	}
-	
-	return res.json({});
+	const result = { 
+		success,
+		data : returnData,
+		message
+	};
+	return res.json(result);
 });
 
 module.exports = router;
