82132a44b0458c1fd60bc03898e70656660b80f3
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-10 18:10:35 +0900

    .

--- exam2/kanbanServer/routes/kanban.js
+++ exam2/kanbanServer/routes/kanban.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 
 router.use((req, res) => {
-	
-	return res.json({});
+	const data = req.body;
+	const mode = data.mode;
+	let success = false;
+	let returnData = {};
+	let message = "";
+	switch (mode) {
+		/** 작업 추가 */
+		case "add": 
+			
+			break;
+		/** 작업 수정 */
+		case "edit" : 
+			break;
+		/** 작업 삭제 */
+		case "delete" :
+			break;
+		/** 작업 목록 */
+		case "getList" : 
+			break;
+	}
+	const _result = {
+		success, 
+		data : returnData,
+		message,
+	};
+	return res.json(_result);
 });
 
 module.exports = router;
