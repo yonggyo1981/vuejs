87db9d4979debe79c26d85e2544e023f1087765d
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-10 17:54:09 +0900

    .

---
+++ exam2/vue-kanban/src/models/kanban.js
@@ -0,0 +1,20 @@
+export default {
+    methods : {
+        /** 작업 추가 */
+        $addWork(data) {
+            console.log(data);
+        },
+        /** 작업 수정 */
+        $editWork(data) {
+            console.log(data);
+        },
+        /** 작업 삭제 */
+        $deleteWork(idx) {
+            console.log(idx);
+        },
+        /** 작업 목록  */
+        $getList(status) {
+            console.log(status);
+        }
+    }
+}
