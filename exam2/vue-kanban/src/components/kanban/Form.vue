7e7cccc71c67a2fc399056e5014c5a660bfc0e67
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-10 16:56:44 +0900

    .

---
+++ exam2/vue-kanban/src/components/kanban/Form.vue
@@ -0,0 +1,37 @@
+<template>
+    <form ref="frmKanban" autocomplete="off" @submit="formSubmit($event)">
+        <dl>
+            <dt>작업구분</dt>
+            <dd>
+                <label>
+                    <input type="radio" name="status" value="ready" v-model="status">
+                    준비중
+                </label>
+                <label>
+                    <input type="radio" name="status" value="progress" v-model="status">
+                    진행중
+                </label>
+                <label>
+                    <input type="radio" name="status" value="done" v-model="status">
+                    완료
+                </label>
+            </dd>
+        </dl>
+        <input type="submit" value="작업등록">
+    </form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            status : "",
+        };
+    },
+    methods : {
+        formSubmit(e) {
+            e.preventDefault();
+            console.log(this.status);
+        }
+    }
+}
+</script>
---
+++ exam2/vue-kanban/src/views/kanban/Add.vue
@@ -0,0 +1,11 @@
+<template>
+    <PageTitle>작업등록</PageTitle>
+    <Form />
+</template>
+<script>
+import PageTitle from "../../components/PageTitle.vue"
+import Form from "../../components/kanban/Form.vue"
+export default {
+    components: { PageTitle, Form },
+}
+</script>
