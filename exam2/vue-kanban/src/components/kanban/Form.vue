7a9671583d8ecacd0d9f557f5b2cf9452a9eea91
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-10 17:06:33 +0900

    .

--- exam2/vue-kanban/src/components/kanban/Form.vue
+++ exam2/vue-kanban/src/components/kanban/Form.vue
@@ -5,15 +5,15 @@
             <dt>작업구분</dt>
             <dd>
                 <label>
-                    <input type="radio" name="status" value="ready" v-model="status">
+                    <input type="radio" name="status" value="ready" v-model="picked">
                     준비중
                 </label>
                 <label>
-                    <input type="radio" name="status" value="progress" v-model="status">
+                    <input type="radio" name="status" value="progress" v-model="picked">
                     진행중
                 </label>
                 <label>
-                    <input type="radio" name="status" value="done" v-model="status">
+                    <input type="radio" name="status" value="done" v-model="picked">
                     완료
                 </label>
             </dd>
@@ -37,13 +37,20 @@
 export default {
     data() {
         return {
-            status : "ready",
+            picked : "ready",
         };
+    },
+    mounted() {
+        this.picked = this.status;
     },
     props : {
         mode : {
             type : String,
             default : "add",
+        },
+        status : {
+            type : String,
+            default : "ready",
         },
         subject : {
             type : String,
