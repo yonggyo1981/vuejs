a149badec7adcc4bee9e4fb13ec9d188a8db7012
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-12 16:47:46 +0900

    .

--- exam2/kanbanServer/models/kanban.js
+++ exam2/kanbanServer/models/kanban.js
@@ -1,10 +1,29 @@
+const { sequelize, Sequelize : { QueryTypes } } = require('./index');
+
 /**
 * 칸반보드 model
 *
 */
 const kanban = {
-	addWork(data) {
-		
+	async addWork(data) {
+		const replacements = {
+				memNo : data.memNo,
+				status : data.status,
+				subject : data.subject,
+				content : data.content,
+		};
+		const sql = `INSERT INTO worklist (memNo, status, subject, content) 
+								VALUES (:memNo, :status, :subject, :content)`;
+		try {
+			const result = await sequelize.query(sql, {
+				replacements,
+				type : QueryTypes.INSERT,
+			});
+			return result[0];
+		} catch (err) {
+			console.error(err);
+			return false;
+		}		
 	},
 	editWork(data) {
 		
--- exam2/kanbanServer/routes/kanban.js
+++ exam2/kanbanServer/routes/kanban.js
@@ -1,26 +1,40 @@
 const router = require('express').Router();
+const kanban = require("../models/kanban");
 
-router.use((req, res) => {
+router.use(async (req, res) => {
 	const data = req.body;
 	const mode = data.mode;
 	let success = false;
 	let returnData = {};
 	let message = "";
-	switch (mode) {
-		/** 작업 추가 */
-		case "add": 
-			
-			break;
-		/** 작업 수정 */
-		case "edit" : 
-			break;
-		/** 작업 삭제 */
-		case "delete" :
-			break;
-		/** 작업 목록 */
-		case "getList" : 
-			break;
+	try {
+		switch (mode) {
+			/** 작업 추가 */
+			case "add": 
+				const idx = await kanban.addWork(data);
+				if (!idx) {
+					throw new Error('작업등록 실패하였습니다.');
+				}
+				
+				success = true;
+				returnData = { idx };
+				break;
+			/** 작업 수정 */
+			case "edit" : 
+				kanban.editWork(data);
+				break;
+			/** 작업 삭제 */
+			case "delete" :
+				break;
+			/** 작업 목록 */
+			case "getList" : 
+				break;
+		}
+	} catch(err) {
+		success = false;
+		message = err.message;
 	}
+	
 	const _result = {
 		success, 
 		data : returnData,
