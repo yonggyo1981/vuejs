240e897d665dbba7dfcc3f0d79ba94f5fbce94b6
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-15 15:49:41 +0900

    .

--- exam2/vue-kanban/src/views/kanban/List.vue
+++ exam2/vue-kanban/src/views/kanban/List.vue
@@ -1,13 +1,20 @@
 <template>
 <PageTitle>작업 목록</PageTitle>
-<List :status='ready' />
-<List :status="progress" />
-<List :status="done" />
+<List :status="status1" />
+<List :status="status2" />
+<List :status="status3" />
 </template>
 <script>
 import PageTitle from "../../components/PageTitle.vue"
 import List from "../../components/kanban/List.vue"
 export default {
     components : {PageTitle, List},
+    data() {
+        return {
+            status1 : "ready",
+            status2 : "progress",
+            status3 : "done"
+        };
+    }
 }
 </script>
