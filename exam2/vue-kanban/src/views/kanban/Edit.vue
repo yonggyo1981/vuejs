37ab2a7b7739b60997f4da5242cbbada23dcfc98
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-15 17:13:46 +0900

    .

---
+++ exam2/vue-kanban/src/views/kanban/Edit.vue
@@ -0,0 +1,26 @@
+<template>
+    <PageTitle>작업수정</PageTitle>
+    <Form :mode="mode" :kanban="kanban" />
+</template>
+<script>
+import PageTitle from "../../components/PageTitle.vue"
+import Form from "../../components/kanban/Form.vue"
+import kanban from "../../models/kanban.js"
+export default {
+    components: { PageTitle, Form },
+    mixins: [kanban],
+    data() {
+        return {
+            mode : "edit",
+            kanban : {},
+        };
+    },
+    async mounted() {
+        const idx = this.$route.query.idx;
+        const result = await this.$get(idx);
+        if (result.success) {
+            this.kanban = result.data;
+        }
+    }
+}
+</script>
