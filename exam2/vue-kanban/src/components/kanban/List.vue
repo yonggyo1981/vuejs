e7fdb6d59081da04b9b9ad030afc4149fa4823b5
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-15 15:36:28 +0900

    .

---
+++ exam2/vue-kanban/src/components/kanban/List.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class='stit'>{{ title }}</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title : "",
+            list : [],
+        };
+    },
+    props : {
+        status : {
+            type : String,
+            default : "ready",
+        }
+    },
+    mounted() {
+        switch(this.status) {
+            case "progress" : 
+                this.title = "진행중";
+                break;
+            case "done" : 
+                this.title = "완료";
+                break;
+            default : 
+                this.title = "준비중";
+        }
+    }
+}
+</script>
---
+++ exam2/vue-kanban/src/views/kanban/List.vue
@@ -0,0 +1,10 @@
+<template>
+<PageTitle>작업 목록</PageTitle>
+
+</template>
+<script>
+import PageTitle from "../../components/PageTitle.vue"
+export default {
+    components : {PageTitle},
+}
+</script>
